f5fccf1c81e1ca6b3e8587b5e759d371de54fc20
Ameglian Major <ameglianmajor@gmail.com>
2015-02-01 23:17:17 -0800

    Utilized apipie's exceptions and error handling to detect missing or invalid parameters and send back meaningful error messages to the user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,31 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  around_filter :apipie_validation_errors_send_bad_request
+
+  def apipie_validation_errors_send_bad_request
+    begin
+      yield
+    rescue Apipie::ParamMissing => e
+      respond_to do |format|
+        debugger
+        handle_response(e, format, :missing)
+      end
+    rescue Apipie::ParamInvalid => e
+      respond_to do |format|
+        handle_response(e, format, :invalid)
+      end
+    end
+  end
+
+    private
+
+    def handle_response(e, format, condition)
+      format.json { render json:
+        { errors: { e.param => [ condition ] } }, status: :bad_request
+      }
+      format.html { render json:
+        { errors: { e.param => [ condition ] } }, status: :bad_request
+      }
+    end
 end
