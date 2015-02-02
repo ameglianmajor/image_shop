4d30136bffc47a175299d86b05e5b143b058a0da
Ameglian Major <ameglianmajor@gmail.com>
2015-02-01 18:40:47 -0800

    Added a spec for the retrieve method and cleaned up the specs.

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe Image, :type => :model do
   before(:each) do
     test_image_path = 'spec/test_files/peace.jpeg'
     full_image_path = Rails.root.join(test_image_path)
-    test_image = ::Magick::Image.read(full_image_path)[0]
-    test_image_url = 'http://www.clipartbest.com/cliparts/KTj/49G/KTj49GATq.jpeg'
+    @test_image = ::Magick::Image.read(full_image_path)[0]
+    @test_image_url = 'http://www.clipartbest.com/cliparts/KTj/49G/KTj49GATq.jpeg'
     @image = Image.new(
       name: "White Dove Peace Picture",
-      image_blob: Base64.encode64(test_image.to_blob),
-      url: URI::encode(test_image_url),
+      image_blob: Base64.encode64(@test_image.to_blob),
+      url: URI::encode(@test_image_url),
       content_type: 'image/jpeq',
       # Time since last Epoch (0:00 on January 1, 1970)
       # Since we are caching and users will not be in the same time zone,
@@ -39,31 +39,49 @@ RSpec.describe Image, :type => :model do
 
   context "when a valid image_blob is present" do
     it "calls resize_image when the resize endpoint is invoked" do
-      new_width = 400
-      new_height = 400
+      new_width = '400'
+      new_height = '400'
       resized_image_blob = @image.resize_image(new_width,new_height)
       resized_image = ::Magick::ImageList.new()
       resized_image.from_blob(resized_image_blob)
-      expect(resized_image.columns).to eq(new_width)
-      expect(resized_image.rows).to eq(new_height)
+      expect(resized_image.columns).to eq(new_width.to_i)
+      expect(resized_image.rows).to eq(new_height.to_i)
     end
 
     it "calls crop_image when the crop endpoint is invoked" do
-      x = 50
-      y = 50
-      new_width = 150
-      new_height = 150
+      upper_left_corner = {x: '50', y: '50'}
+      new_width = '150'
+      new_height = '150'
       cropped_image_blob =
-        @image.crop_image({x: x, y: y}, new_width, new_height)
+        @image.crop_image(upper_left_corner, new_width, new_height)
       cropped_image = ::Magick::ImageList.new()
       cropped_image.from_blob(cropped_image_blob)
-      expect(cropped_image.columns).to eq(new_width)
-      expect(cropped_image.rows).to eq(new_height)
+      expect(cropped_image.columns).to eq(new_width.to_i)
+      expect(cropped_image.rows).to eq(new_height.to_i)
     end
   end
 
   context "when a valid image_blob is not present" do
+    it "retrieves the image from the url" do
+      image_width = 367
+      image_height = 350
 
+      stub_request(:get, @test_image_url).
+        with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v0.9.1'}).
+        to_return(
+          status: :ok,
+          body: @test_image.to_blob,
+          :headers => { 'Content-Type' => 'image/jpeg' }
+        )
+
+      retrieved_image_model = Image.retrieve(@test_image_url)
+      retrieved_image = ::Magick::ImageList.new()
+      retrieved_image.from_blob(
+        Base64.decode64(retrieved_image_model.image_blob)
+      )
+      expect(retrieved_image.columns).to eq(image_width)
+      expect(retrieved_image.rows).to eq(image_height)
+    end
   end
 
 end
